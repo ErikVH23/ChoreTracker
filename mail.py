3ed47409a9e9588ca1a8fd621bd18bde9bd0443f
ErikVH23 <eavanhor@mtu.edu>
2021-02-05 22:57:49 -0500

    yoink

--- mail.py
+++ mail.py
@@ -1,10 +1,11 @@
 import smtplib, ssl
-import getpass
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import json
 from jinja2 import Environment, FileSystemLoader
 import os
+import getpass
+import sys
 
 PORT = 500
 SMPT_SERVER = "smpt.gmail.com"
@@ -22,22 +23,22 @@ def sendMessage():
         server = smtplib.SMTP('smtp.gmail.com',587)
         server.starttls()
         server.ehlo()
-        server.login(SENDER_EMAIL, 'williams19905')
+        password = getpass.getpass(stream=sys.stderr)
+        server.login(SENDER_EMAIL, password)
 
         #generates list of name
         names = []
         for n in users["Users"]:
-            print(n["Name"])
             names.append(n["Name"])
 
-        generateHTML(names)
+        generateHTML(createList(users))
 
-        for name in test:
+        for user in users["Users"]:
             try:
-                server.sendmail(SENDER_EMAIL, name, createMessage(name))
-                print("Sent to {}".format(name))
+                server.sendmail(SENDER_EMAIL, user["Email"], createMessage(user["Name"]))
+                print("Sent to {}".format(user["Name"]))
             except Exception as e:
-                print("Could not send to {}".format(name))
+                print("Could not send to {}".format(user["Name"]))
                 print(e)
 
     except Exception as e:
@@ -46,6 +47,9 @@ def sendMessage():
         print('Server closed')
 
 def createMessage(name: str) -> str:
+    """
+
+    """
     msg = MIMEMultipart('alternative')
     msg['Subject'] = "Chores"
     msg['From'] = SENDER_EMAIL
@@ -58,23 +62,67 @@ def createMessage(name: str) -> str:
 
     return msg.as_string()
 
-#Generates HTML file based on chorelist
-def generateHTML(choreList: dict) -> str:
+def generateHTML(choreList: dict):
+    """
+    Takes in a dict containg chores, writes to out.html
+    based on the template email.html 
+
+    Parameters
+    ----------
+    choreList: dict, required
+
+    Returns
+    -------
+    None
+    """
     env = Environment(loader=FileSystemLoader('./templates/'))
     template = env.get_template('email.html')
+
 
     with open('./html/out.html','w') as fh:
         fh.write(template.render(
             chores = choreList
         ))
 
-
-#Function to create name|chore pairs for the week
+"""ANDERS THIS IS WHAT YOU NEED TO DO"""
 def createList(users: dict) -> list:
-    pass
+    """
+    Generates list of tuples pairing individuals with their weekly
+    chores. Takes into account circumstances such as floor, cat duty
+    and special (Basically if you are Jacob or not)
+
+    Parameters
+    ----------
+    users: dict
+    Returns
+    -------
+    choreList: list
+    """
+
+    chores = []
+    people = []
+
+    for person in users["Users"]:
+        people.append(person["Name"])
+    for chore in users["Chores"]:
+        chores.append(chore["Title"]) 
+
+    #print("People: {}".format(len(people)))
+    #print("Chores: {}".format(len(chores)))
+    choreList = []
+
+    for x in people:
+        if len(chores) == 0:
+            break
+        else:
+            choreList.append(x+"   -   "+chores.pop())
+
+
+    return choreList 
+
 
 
 
 
 if __name__ == "__main__":
-    sendMessage()    
+    generateHTML(createList(users))
